caee6c7c1d450ed6647a607a50fb7597a0b2b171
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-07-16 13:02:12 +0530

    Delete functionality completed.

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { getLeads } from "../../actions/leads";
+import { getLeads, deleteLead } from "../../actions/leads";
 
 export class Leads extends Component {
   static propTypes = {
@@ -14,9 +14,39 @@ export class Leads extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Leads List</h1>
-      </div>
+      <Fragment>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map((lead) => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button
+                    onClick={this.props.deleteLead.bind(this, lead.id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    {" "}
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     );
   }
 }
@@ -25,4 +55,4 @@ const mapStateToProps = (state) => ({
   leads: state.leads.leads,
 });
 
-export default connect(mapStateToProps, { getLeads })(Leads);
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads);
