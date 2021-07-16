2f501c4bc58c935114468cca84dfbdbea3d1a38f
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2021-07-16 22:17:18 +0530

    Token Authentication is completed.

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -30,4 +30,16 @@ class LoginAPI(generics.GenericAPIView):
             'user': UserSerializer(user, context=self.get_serializer_context()).data,
             'token': AuthToken.objects.create(user)[1]
         })
+
 # GET User API
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
